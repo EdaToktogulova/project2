290ce5be2857060866a20b7095e98a33c67777b0
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-05-10 18:16:48 +0600

    import react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Navbar from "./components/navbar/Navbar";
 
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+    <Navbar />
+    </>
   );
 }
 
